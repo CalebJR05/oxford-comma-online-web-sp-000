0df1cb0c8b0357e7f4a591daee97f73f7498a23f
Caleb R <cjr4912@gmail.com>
2020-07-22 20:13:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,11 @@
 def oxford_comma(array)
-array.join
-array.join(" and ")
-  
+  if array.length == 2
+  array[-2] << " and "
+  array.join
+  elsif array.length == 1
+  	array.join
+  elsif array.length > 2
+    array[-1].unshift "and "
+	array.join(", ")
+  end
 end
