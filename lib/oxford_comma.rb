94a1904b814386af30a19c0883219d58f4f78611
Caleb R <cjr4912@gmail.com>
2020-07-22 20:05:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
 array.join
 array.join(" and ")
 array.each.do |fruit|
-  puts fruit " , " fruit " , and " fruit
+puts fruit " , " fruit " , and " fruit
 array.join(" and ")
   
   end
